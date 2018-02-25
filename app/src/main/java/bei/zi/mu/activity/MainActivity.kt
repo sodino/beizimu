5a2dac569179e445161e8ade134c4e393bd8626f
cht <cht@meitu.com>
2018-02-25 21:49:38 +0800

    【开发】初始化工程

---
+++ app/src/main/java/bei/zi/mu/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package bei.zi.mu.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import bei.zi.mu.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
